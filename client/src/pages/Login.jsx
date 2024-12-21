bf060154971658f46d0c401dc3fc265768e7acb0
T.DAMAN <98676229+damantadi@users.noreply.github.com>
2024-12-21 06:41:42 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
        if(data.success){
         setIsLoggedin(true)
-        getUserData()
+        await getUserData()
         navigate('/')
        }
        else
@@ -37,7 +37,7 @@ const Login = () => {
 
         if(data.success){
          setIsLoggedin(true)
-         getUserData()
+         await getUserData()
          navigate('/')
         }
         else
