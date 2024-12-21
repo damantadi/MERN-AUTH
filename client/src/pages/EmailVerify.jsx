bec204749e909b9205fb6aa43cf12d62ca32a31e
T.DAMAN <98676229+damantadi@users.noreply.github.com>
2024-12-21 10:22:07 +0530

    Update EmailVerify.jsx

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -43,7 +43,7 @@ const EmailVerify = () => {
      if(data.success)
      {
       toast.success(data.message)
-      await getUserData()
+      getUserData()
       navigate('/')
      }
       else
